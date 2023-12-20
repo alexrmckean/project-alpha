d8d7fac4cac3f790f0a0a4b711a86ebde4e047ee
Alex <alexrmckean@gmail.com>
2023-12-20 11:36:18 -0500

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,6 @@ from projects.models import Project
 from django.contrib.auth.decorators import login_required
 
 
-
 # Create your views here.
 @login_required
 def show_project_list(request):
@@ -12,3 +11,12 @@ def show_project_list(request):
     "project_list": project_list,
   }
   return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_project(request, id):
+  project = Project.objects.get(id=id)
+  context = {
+    "project": project,
+  }
+  return render(request, "projects/detail.html", context)
